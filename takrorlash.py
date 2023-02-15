a12a93a9180c846e73510d091821eefc49e4377a
Maqsudkhan <maqsudtorayev2003@gmail.com>
2023-02-16 04:50:07 +0500

    Create takrorlash.py

---
+++ takrorlash.py
@@ -0,0 +1,212 @@
+# Bismillahir rohmanir rohiym
+
+
+#print('"Nexia"' , '"Tico"' , "'Damas'"  , "ko\'rganlar \nqilar havas" )
+#print('5 ning 4-darajasi',5**4)
+#print('22 ni 4 ga bo\'lgandagi qoldiq' , 22%4)
+#print("Tomonlari 125 ga teng bolgan kvadratning yuzi" , 125*125 , "ga va perimetri" , 125*4 , "ga teng")
+#print("Deametri 12 ga teng bo'lgan doiraning yuzi" , 3.14*6**2 , "ga teng")
+#print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi " ,(6**2+7**2)**0.5)
+
+yangi = "hello world"
+#print(yangi)
+xabar = "Assalom alaykum yaxshimisiz?"
+#print(xabar)
+xabar="Yaxshi"
+#print(xabar)
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi*radius**2
+#print("Radiusi" , radius , "ga teng aylananing yuzi=" , aylana_yuzi)
+matn = "men yangi 📱 oldim"
+#print(matn)
+#ism = "maqsud"
+#print("mening ismim " + ism)
+#familiya = "Torayev"
+#print(familiya +" "+ ism)
+#ism_sharif = f"{ism} {familiya}"
+#print(ism_sharif)
+#print(f"Salom,mening ismim {ism}. {familiya} {ism}
+#print('Hello World')
+#print('Hello \tWorld')
+#print('Hello \nWorld')
+#print(ism_sharif.upper())
+#print(ism_sharif.lower())
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+#print('Maqsud torayev'.upper())
+#print('Assalom Alaykum'.lower())
+#print('assalom alaykum, maqsud'.title())
+#print('assalom alaykum, maqsud'.capitalize())
+meva = '    olma      '
+#print('Men ' + meva.lstrip() + ' yaxshi kuraman')
+#print('Men ' + meva.rstrip() + ' yaxshi kuraman')
+#print('Men ' + meva.strip() + ' yaxshi kuraman')
+
+#name = input("Ismingiz nima? ")
+#print("Assalom alaykum, " + name)
+
+#Name = input("Ismingiz nima?\n>>>")
+#print("Assalom alaykum, " + Name.title())
+ 
+kucha="Bog'bon"
+mahalla="Sag'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+#print(kucha + " kuchasi, " + mahalla + "\nmahallasi, " + tuman + " tumani,\n" + viloyat + " viloyati.")
+
+#kucha=input("Viloyatingiz?>>> ") +input("Tumaningiz?>>> ") + input("Mahallangiz?>>> ") + input("Kuchangiz?>>> ")
+
+#kucha=input("Kuchangiz?>>> ")
+#mahalla=input("Mahallangiz?>>> ")
+#tuman=input("Tumaningiz?>>> ")
+#viloyat=input("Viloyatingiz?>>> ")
+#print(kucha + " kuchasi,\n" + mahalla + " mahallasi,\n" + tuman + " tumani,\n" + viloyat + " viloyati.")
+
+manzil=f"{kucha} kuchasi {mahalla} mahallasi {tuman} tumani {viloyat} viloyati."
+#print(manzil)
+#print(manzil.upper())
+#print(manzil.lower())
+#print(manzil.title())
+#print(manzil.capitalize())
+
+#ism="Maqsud"
+#yosh=20
+#xabar=ism + ' ' + str(yosh) + " yoshda."
+#print(xabar)
+
+#print(type(ism))
+#print(type(yosh))
+
+#t_yil=input("Tug'ulgan yilingizni kiriting: ")
+#t_yil=int(t_yil)
+#yosh=2023-t_yil
+#print("Siz " + str(yosh) + " yoshda ekansiz!")
+
+#yosh=input("Yoshingiz nechchida: ")
+#yosh=int(yosh)
+#age=2023-yosh
+#print("Siz " + str(age) + "-yilda tug'ulgansiz.")
+
+#son1=input("birinchi sonni kiriting: ")
+#son2=input("ikkinchi sonni kiriting: ")
+#print(float(son1) , '+' , float(son2) , '=' , float(son1) + float(son2))
+#print(float(son1) , '-' , float(son2) , '=' , float(son1) - float(son2))
+#print(float(son1) , ':' , float(son2) , '=' , float(son1) / float(son2))
+#print(float(son1) , 'x' , float(son2) , '=' , float(son1) * float(son2))
+
+#son=input('Istalgan sonni kiriting:\n>>>')
+#print(son , 'ning kvadrati' , float(son)**2 , 'ga teng.')
+#print(son , 'ning kubi' , float(son)**3 , 'ga teng.')
+
+mevalar=['olma', 'anor' , 'shaftoli' , 'nok' , 'behi' ]
+mevalar.append('qovun')
+del mevalar[1]
+names=['maxmud','shuhrat','dilshod','asad']
+
+sonlar=[12,23.456,-34,45,-67,-89,45.89,-15.3]
+#print(sonlar[2]+sonlar[7])
+#print((sonlar[5]/sonlar[4]))
+#print(sonlar[0]**3)
+sonlar[1]=25.5
+sonlar.append(100)
+sonlar.insert(2,1000)
+#print(sonlar)
+
+t_shaxslar=["Alisher Navoiy" , "Mirzo Ulug'bek" , "Amir Temur" , "Ibn Sino"]
+z_shaxslar=["Elon Maask" , "Stev Jobs" , "Bill Gates" , "Maqsudkhan"]
+#print("Men tarixiy shaxslardan " + t_shaxslar.pop(2)+ " bilan, zamanoviy shaxslardan esa " + z_shaxslar.pop(1)+ " bilan suhbat qilishni istardim!!!")
+
+
+
+
+
+
+
+
+
+
+friends=[]
+friends.append('Dilshod')
+friends.append('Shuhrat')
+friends.append('Asliddin')
+friends.append('Maxmud')
+friends.append('Asilbek')
+friends.append('Asad')
+friends.append('Yusuf')
+
+friends.remove('Maxmud')
+friends.remove('Yusuf')
+friends.remove('Asad')
+#print(friends)
+friends.insert(4,'Muhammad')
+friends.insert(0,'Erajbek')
+friends.insert(3,'Jahongir')
+print(friends)
+
+mehmonlar=[]
+
+mehmonlar=friends.pop(1)
+mehmonlar=friends.pop(1)
+mehmonlar=friends.pop(2)
+mehmonlar=friends.pop(2)
+print("kemagan mehmonlar:" ,friends)
+
+
+mehmonlar.append('Dilshod')
+mehmonlar.append('Shuhrat')
+mehmonlar.append('Asliddin')
+mehmonlar.append('Asilbek')
+
+print("kelgan mehmonlar:" ,mehmonlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
